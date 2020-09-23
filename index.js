548ff298bd5cd502b8d5135fec288ce96403779f
JenIglar <jeniglar@gmail.com>
2020-09-23 15:24:12 -0400

    set const questions

--- index.js
+++ index.js
@@ -1,7 +1,43 @@
+const inquirer = require("inquirer");
+
+require("./utils/generateMarkdown");
 // array of questions for user
 const questions = [
-
+    {type: "input",
+     name: "title",
+     message: "Please enter your project title"
+    },
+    {type: "input",
+     name: "description",
+     message: "Please enter a description of your project"
+    },
+    {type: "input",
+     name: "installation",
+     message: "Please enter installation instructions"
+    },
+    {type: "input",
+     name: "usage",
+     message: "Please enter usage information"
+    },
+    {type: "list",
+     name: "license",
+     message: "?"
+    },
+    {type: "input",
+     name: "contributions",
+     message: "Please enter contribution guidelines"
+    },
+    {type: "input",
+     name: "tests",
+     message: "Please enter test instructions"
+    },
+    {type: "input",
+     name: "questions",
+     message: "Please enter your email address"
+    }
 ];
+
+inquirer.prompt(questions).then()
 
 // function to write README file
 function writeToFile(fileName, data) {
